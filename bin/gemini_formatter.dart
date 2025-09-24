4a7f81c994130e1974c4588139c85adcf7d5995c
Dev Ttangkong <ttankkeo112@gmail.com>
2025-09-25 05:18:34 +0900

    Fixed an issue about package directory path

--- bin/gemini_formatter.dart
+++ bin/gemini_formatter.dart
@@ -5,9 +5,28 @@ import 'package:gemini_formatter/source_file.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:gemini_formatter/source_file_binding.dart';
 import 'package:yaml/yaml.dart';
+import 'package:path/path.dart';
 
+/// Cleans JSON-like text by removing Markdown code fences.
 String cleanJson(String text) {
   return text.replaceAll(RegExp(r'```[a-zA-Z]*\n?'), '').replaceAll('```', '');
+}
+
+/// Recursively searches parent directories starting from [startDir] to find 
+/// the package root directory. (i.e., the directory containing pubspec.yaml).
+Directory findPackageRoot(Directory startDir) {
+  var dir = startDir;
+  while (true) {
+    final pubspec = File(join(dir.path, 'pubspec.yaml'));
+    if (pubspec.existsSync()) {
+      return dir;
+    }
+    if (dir.parent.path == dir.path) {
+      // 루트까지 올라왔는데도 pubspec.yaml 없음
+      throw Exception('패키지 루트를 찾을 수 없습니다.');
+    }
+    dir = dir.parent;
+  }
 }
 
 void main(List<String> arguments) {
@@ -38,7 +57,8 @@ void main(List<String> arguments) {
   final promptsDir = Directory(config["promptsDir"]);
   final promptsFiles = SourceFileBinding.load(promptsDir);
 
-  final constraintsDir = Directory("../prompts");
+  final packageDir = findPackageRoot(Directory(Platform.script.toFilePath()));
+  final constraintsDir = Directory(join(packageDir.path, "./prompts"));
   final constraintsFiles = SourceFileBinding.load(constraintsDir);
 
   () async {
